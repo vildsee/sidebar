2b2db3b943a393c11706eb5c67fa673aa1f2a747
vildsee <78222196+vildsee@users.noreply.github.com>
2022-10-15 22:23:29 +0200

    finished

---
+++ app.js
@@ -0,0 +1,25 @@
+// .sidebar
+// .sidebar-toggle
+// .close-btn
+
+const toggleBtn = document.querySelector(".sidebar-toggle");
+const closeBtn = document.querySelector(".close-btn");
+const sidebar = document.querySelector(".sidebar");
+
+toggleBtn.addEventListener("click", function () {
+  //console.log(sidebar.classList);
+  //   if (sidebar.classList.contains("show-sidebar")) {
+  //     sidebar.classList.remove("show-sidebar");
+  //   } else {
+  //     sidebar.classList.add("show-sidebar");
+  //   }
+  sidebar.classList.toggle("show-sidebar");
+});
+
+closeBtn.addEventListener("click", function () {
+  //   console.log(closeBtn.classList);
+  //   if (sidebar.classList.contains("show-sidebar")) {
+  //     sidebar.classList.remove("show-sidebar");
+  //   }
+  sidebar.classList.toggle("show-sidebar");
+});
